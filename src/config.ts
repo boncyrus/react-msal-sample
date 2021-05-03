f8024aa0508d89a3268b33ac73252271b9f8c39b
Bon Macalindong <macalindong_bon23@outlook.com>
2021-05-03 22:51:19 +0800

    Updated readme and config

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import { Configuration } from "@azure/msal-browser";
 export const azureAdConfig: Configuration = {
     auth: {
         clientId: "62b76b1d-22bd-4127-8398-0fc0b6570e10",
-        authority: "https://login.microsoftonline.com/bonccmacalindongoutlook.onmicrosoft.com",
+        authority: "https://login.microsoftonline.com/cb0df0a9-0c65-493b-80cf-efba788a2604",
         redirectUri: "http://localhost:3000",
         postLogoutRedirectUri: "http://localhost:3000"
     },
